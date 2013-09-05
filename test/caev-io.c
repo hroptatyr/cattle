164811c02b20c8cdbf8c59f72a385f9b9fe89816
Sebastian Freundt <freundt@ga-group.nl>
2013-09-05 12:50:05 +0000

    minor, provide printer for funds in caev-io test harness
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- test/caev-io.c
+++ test/caev-io.c
@@ -3,12 +3,21 @@
 #include "cattle.h"
 #include "caev.h"
 
-static void
+static inline void
 ctl_caev_pr(ctl_caev_t e)
 {
 	printf("m: %d<-%u + %f  ", e.mktprc.r.p, e.mktprc.r.q, (float)e.mktprc.a);
 	printf("n: %d<-%u + %f  ", e.nomval.r.p, e.nomval.r.q, (float)e.nomval.a);
 	printf("o: %d<-%u + %f\n", e.outsec.r.p, e.outsec.r.q, (float)e.outsec.a);
+	return;
+}
+
+static inline void
+ctl_fund_pr(ctl_fund_t f)
+{
+	printf("m: %f  ", (float)f.mktprc);
+	printf("n: %f  ", (float)f.nomval);
+	printf("o: %f\n", (float)f.outsec);
 	return;
 }
 
