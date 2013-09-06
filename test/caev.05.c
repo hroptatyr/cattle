2da706e1e3c059844da1a9cb279398d518c2c56a
Sebastian Freundt <freundt@ga-group.nl>
2013-09-06 11:26:59 +0000

    test, reciprocate the events twice before memcmp()ing them
    0.df is not the same as a binary 0 buffer.
    
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- test/caev.05.c
+++ test/caev.05.c
@@ -16,9 +16,14 @@ main(void)
 	ctl_caev_pr(rev);
 	ctl_caev_pr(r2v);
 
+	/* unroll more, so we dont suffer from initialisation problems */
+	rev = ctl_caev_rev(r2v);
+	dvca = ctl_caev_rev(rev);
+
 	/* we use the fact that rev is an involution */
 	if (memcmp(&dvca, &r2v, sizeof(dvca))) {
-		/* r cell should be null_ratio */
+		ctl_aux_hexpr(&dvca, sizeof(dvca));
+		ctl_aux_hexpr(&r2v, sizeof(r2v));
 		res = 1;
 	}
 	return res;
