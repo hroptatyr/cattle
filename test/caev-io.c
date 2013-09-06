81561324e06cc99992a570e467357f925b4cdc35
Sebastian Freundt <freundt@ga-group.nl>
2013-09-06 11:24:40 +0000

    minor, support hex printing of buffers
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- test/caev-io.c
+++ test/caev-io.c
@@ -1,4 +1,5 @@
 /* just some routines for caev_t printing */
+#include <stdint.h>
 #include <stdio.h>
 #include "cattle.h"
 #include "caev.h"
@@ -18,6 +19,18 @@ ctl_fund_pr(ctl_fund_t f)
 	printf("m: %f  ", (float)f.mktprc);
 	printf("n: %f  ", (float)f.nomval);
 	printf("o: %f\n", (float)f.outsec);
+	return;
+}
+
+static __attribute__((unused)) void
+ctl_aux_hexpr(const void *buf, size_t bsz)
+{
+	static char hx[] = "0123456789abcdef";
+	for (const uint8_t *p = buf, *const ep = p + bsz; p < ep; p++) {
+		putchar(hx[(*p & 0xf0U) >> 4U]);
+		putchar(hx[(*p & 0x0fU) >> 0U]);
+	}
+	putchar('\n');
 	return;
 }
 
