870a38d7423db417be927e05f3a792e0f2adf42f
Sebastian Freundt <freundt@ga-group.nl>
2013-09-24 15:15:57 +0000

    chore, build glue for cattle(1) and libcoru coroutines
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ src/coru.h
@@ -0,0 +1,67 @@
+/*** coru.h -- coroutines
+ *
+ * Copyright (C) 2013 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of cattle.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+#if !defined INCLUDED_coru_h_
+#define INCLUDED_coru_h_
+
+#include "coru/cocore.h"
+
+#define PREP()		initialise_cocore_thread()
+#define UNPREP()	terminate_cocore_thread()
+#define START(x, ctx)							\
+	({								\
+		struct cocore *next = (ctx)->next;			\
+		create_cocore(						\
+			next, (cocore_action_t)(x),			\
+			(ctx), sizeof(*(ctx)),				\
+			next, 0U, false, 0);				\
+	})
+#define NEXT1(x, o)	(check_cocore(x) ? switch_cocore((x), (o)) : NULL)
+#define NEXT(x)		NEXT1(x, NULL)
+#define YIELD(x)	switch_cocore(CORU_CLOSUR(next), (void*)(intptr_t)(x))
+
+#define DEFCORU(name, closure, arg)			\
+	struct name##_s {				\
+		struct cocore *next;			\
+		struct closure;				\
+	};						\
+	static void *name(struct name##_s *coru_ctx, arg)
+#define CORU_CLOSUR(x)	(coru_ctx->x)
+#define CORU_STRUCT(x)	struct x##_s
+#define PACK(x, args...)	&((CORU_STRUCT(x)){args})
+#define START_PACK(x, args...)	START(x, PACK(x, args))
+
+#endif	/* INCLUDED_coru_h_ */
