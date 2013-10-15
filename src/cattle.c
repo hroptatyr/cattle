f690a3ba0cf01134911723355884faa1fa09a595
Sebastian Freundt <freundt@ga-group.nl>
2013-10-15 07:51:17 +0000

    minor, actually initialise the sum slot of ctx to zero explicitly
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/cattle.c
+++ src/cattle.c
@@ -347,6 +347,8 @@ cmd_print(struct ctl_args_info argi[static 1U])
 		res = 1;
 		goto out;
 	}
+	/* initialise sum to some zero */
+	ctx->sum = ctl_zero_caev();
 
 	for (unsigned int i = 1U; i < argi->inputs_num; i++) {
 		const char *fn = argi->inputs[i];
@@ -398,6 +400,8 @@ cmd_apply(struct ctl_args_info argi[static 1U])
 	if (argi->reverse_given) {
 		ctx->rev = 1U;
 	}
+	/* initialise sum to some zero */
+	ctx->sum = ctl_zero_caev();
 
 	/* open caev file and read */
 	with (const char *caev_fn = argi->inputs[2U]) {
