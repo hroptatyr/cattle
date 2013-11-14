f22c61c19c18c5b45382f724b50a1a9cf8d0d6ef
Sebastian Freundt <freundt@ga-group.nl>
2013-11-14 12:02:43 +0000

    fix, use d32tostr() routines in caev-io.c for the tests
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- test/caev-io.c
+++ test/caev-io.c
@@ -17,11 +17,11 @@ ctl_caev_pr(ctl_caev_t e)
 {
 	char buf[32U];
 
-	bid32tostr(buf, sizeof(buf), e.mktprc.a);
+	d32tostr(buf, sizeof(buf), e.mktprc.a);
 	printf("m: %d<-%u + %s  ", e.mktprc.r.p, e.mktprc.r.q, buf);
-	bid32tostr(buf, sizeof(buf), e.nomval.a);
+	d32tostr(buf, sizeof(buf), e.nomval.a);
 	printf("n: %d<-%u + %s  ", e.nomval.r.p, e.nomval.r.q, buf);
-	bid32tostr(buf, sizeof(buf), e.outsec.a);
+	d32tostr(buf, sizeof(buf), e.outsec.a);
 	printf("o: %d<-%u + %s\n", e.outsec.r.p, e.outsec.r.q, buf);
 	return;
 }
@@ -31,11 +31,11 @@ ctl_fund_pr(ctl_fund_t f)
 {
 	char buf[32U];
 
-	bid32tostr(buf, sizeof(buf), f.mktprc);
+	d32tostr(buf, sizeof(buf), f.mktprc);
 	printf("m: %s  ", buf);
-	bid32tostr(buf, sizeof(buf), f.nomval);
+	d32tostr(buf, sizeof(buf), f.nomval);
 	printf("n: %s  ", buf);
-	bid32tostr(buf, sizeof(buf), f.outsec);
+	d32tostr(buf, sizeof(buf), f.outsec);
 	printf("o: %s\n", buf);
 	return;
 }
