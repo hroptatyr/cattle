56e49f56f144f8c7a5e17b1d0eb286fb73fa74a7
Sebastian Freundt <freundt@ga-group.nl>
2015-02-08 09:10:02 +0000

    minor, describe arithmetics and implement them accordingly

--- src/caev.c
+++ src/caev.c
@@ -53,7 +53,72 @@
  * must obey chronological order.  Same for SUB, the latest CAEV added
  * must be subbed first (LIFO), this is the right inverse of ADD, called
  * ctl_caev_sub();  to get the left inverse of ADD, respectively, the
- * FIFO-sub, use ctl_caev_sup(). */
+ * FIFO-sub, use ctl_caev_sup().
+ *
+ * The arithmetic we implement here favours additive operations over
+ * multiplicative.  In our (raw) syntax we will write  x+p<-q  meaning
+ * add x to the price, then, if p is non-zero, multiply by p, then for
+ * non-zero q divide by q.
+ *
+ * Add for additive operations is defined as:
+ *
+ *     x1+0<-0 + x2+0<-0 = (x1+x2)+0<-0
+ *
+ * and hence the inverse of x+0<-0 is (-x)+0<-0.
+ *
+ * Similarly, add for multiplicative operations is defined as:
+ *
+ *    0+p1<-q1 + 0+p2<-q2 = 0+(p1p2)<-(q1q2)
+ *
+ * which makes 0+q<-p the inverse of 0+p<-q, with 0+1<-1 being the
+ * neutral element.
+ *
+ * For mixed operations the precedence rule means:
+ *
+ *     x1+p1<-q1 + x2+p2<-q2 = (x1+x2q1/p1)+(p1p2)<-(q1q2)
+ *
+ * So a dividend before a split becomes
+ *
+ *     d+0<-0 + 0+p<-q = d+p<-q
+ *
+ * and a split before a dividend is summed as
+ *
+ *     0+p<-q + d+0<-0 = (dq/p)+p<-q
+ *
+ * This arithmetic satisfies dilution properties just properly,
+ * a dividend of 4 paid before a 2:1 split is the same as a
+ * dividend of 2 paid after a 2:1 split:
+ *
+ *     10@20         10@20
+ *     20@10         10@16 + 10*4
+ *     20@8 + 20*2   20@8  + 10*4
+ *     =160 + 40     =160  + 40
+ *
+ * And so DVCA/4 + SPLF/2:1 = SPLF/2:1 + DVCA/2 or in raw syntax
+ * -4+0<-0 + 0+1<-2  = -4+1<-2  vs  0+1<-2 + -2+0<-0 = -4+1<-2
+ *
+ * From this the 2 inverses of + become apparent, since the additive
+ * part is always absolute in value, subtraction of additive operations
+ * from the left is simply an addition.
+ *
+ *     x1+p1<-q1 -l x2+p2<-q2 = (x1 - x2)+p1/p2<-q1/q2
+ *
+ * And since all multiplicative operations are aggregated into the
+ * multiplicative cell of a sum subtraction of additive operations from the
+ * right is defined as:
+ *
+ *     x1+p1<-q1 -r x2+p2<-q2 = (x1 - q2x2/p2)+p1/p2<-q1/q2
+ *
+ * The actual inverse elements can be computed via left or right
+ * subtraction from the neutral operation 0+0<-0:
+ *
+ *     0+0<-0  -l  x+p<-q = (-x)+q<-p
+ *
+ * which is called the rev or left-inverse, and
+ *
+ *     0+0<-0  -r  x+p<-q = (-qx/p)+q<-p
+ *
+ * respectively, which we simply call inv or right-inverse. */
 
 
 static __attribute__((const, pure)) ctl_price_t
@@ -121,7 +186,7 @@ price_actor_add(ctl_price_actor_t x, ctl_price_actor_t y)
 {
 	ctl_price_actor_t res = {
 		.r = ratio_add(x.r, y.r),
-		.a = price_add(ratio_act_p(ratio_rev(y.r), x.a), y.a),
+		.a = price_add(x.a, ratio_act_p(ratio_rev(x.r), y.a)),
 	};
 	return res;
 }
@@ -131,7 +196,7 @@ quant_actor_add(ctl_quant_actor_t x, ctl_quant_actor_t y)
 {
 	ctl_quant_actor_t res = {
 		.r = ratio_add(x.r, y.r),
-		.a = quant_add(ratio_act_q(ratio_rev(y.r), x.a), y.a),
+		.a = quant_add(x.a, ratio_act_p(ratio_rev(x.r), y.a)),
 	};
 	return res;
 }
@@ -199,9 +264,9 @@ ctl_caev_t
 ctl_caev_sub(ctl_caev_t x, ctl_caev_t y)
 {
 	ctl_caev_t res = {
-		.mktprc = price_actor_add(x.mktprc, price_actor_rev(y.mktprc)),
-		.nomval = price_actor_add(x.nomval, price_actor_rev(y.nomval)),
-		.outsec = quant_actor_add(x.outsec, quant_actor_rev(y.outsec)),
+		.mktprc = price_actor_add(x.mktprc, price_actor_inv(y.mktprc)),
+		.nomval = price_actor_add(x.nomval, price_actor_inv(y.nomval)),
+		.outsec = quant_actor_add(x.outsec, quant_actor_inv(y.outsec)),
 	};
 	return res;
 }
