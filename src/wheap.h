d008a279dcc2c0e03c1ca2237309786c5e854a02
Sebastian Freundt <freundt@ga-group.nl>
2015-02-18 09:10:41 +0000

    ego, update copyright years

--- src/wheap.h
+++ src/wheap.h
@@ -1,6 +1,6 @@
 /*** wheap.h -- weak heaps, as stolen from sxemacs
  *
- * Copyright (C) 2007-2013 Sebastian Freundt
+ * Copyright (C) 2007-2015 Sebastian Freundt
  *
  * Author:  Sebastian Freundt <freundt@ga-group.nl>
  *
