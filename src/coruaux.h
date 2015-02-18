236835383d04519894e70dd31bbd910ff0fd227d
Sebastian Freundt <freundt@ga-group.nl>
2015-02-18 10:44:15 +0000

    chore, spin off coruaux.h from coru.h

---
+++ src/coruaux.h
@@ -0,0 +1,52 @@
+/*** coruaux.h -- coroutines auxiliaries
+ *
+ * Copyright (C) 2013-2015 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of cattle.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+#if !defined INCLUDED_coruaux_h_
+#define INCLUDED_coruaux_h_
+
+#define declcoru(name, ia, in)			\
+	struct name##_initargs_s ia;		\
+	struct name##_args_s in
+#define coru_args(name)		struct name##_args_s
+#define coru_initargs(name)	struct name##_initargs_s
+#define pack_args(name, _args...)	((coru_args(name)){_args})
+#define pack_initargs(name, _args...)	((coru_initargs(name)){_args})
+#define coru_argp(name)		const coru_args(name)*
+#define coru_initargp(name)	const coru_initargs(name)*
+#define defcoru(name, ia, in)	name(coru_argp(name) in, coru_initargp(name) ia)
+#define _defcoru(name, ia, xin)	name(xin, coru_initargp(name) ia)
+
+#endif	/* INCLUDED_coruaux_h_ */
